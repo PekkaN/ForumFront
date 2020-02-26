de22e41de5561b4217de49d0aef5cbb7fdd5860d
PekkaAcademy <pm.nordberg@gmail.com>
2020-02-26 16:25:30 +0100

    refucktoring

--- src/AllThreadsPage.js
+++ src/AllThreadsPage.js
@@ -1,4 +1,3 @@
-import ReactDOM from 'react-dom';
 import React, {Component} from 'react';
 import './Apps.css';
 import signInLogo from './sign-in-alt-solid.svg';
@@ -86,25 +85,7 @@ const threads = [
 const Header = (props) => <h2 className={props.className}>{props.text}</h2>
 const Button = (props)=><button className = {props.className}>{props.name}<img src={props.src} className= {props.imageClass}/></button>
 
-const NavBar = ()=> {
-    return (
-        <div className='Nav'>
-            <Button className = 'HomeButton' src={homePageLogo} imageClass='HomeLogo'/>
-            <MainHeader className='MainHeader' text = 'Academy Foorumi'/>
-            <Button  className = 'LoginButton' name = 'Kirjaudu' src={signInLogo} imageClass='signInLogo'/>
-        </div>
-    )
-}
 const MainHeader = (props)=> <h1 className={props.className}>{props.text}</h1>
-
-/*const Topic = ({name,description}) => {
-    return (
-        <div className='Topic'>
-            <h3 className='TopicName'>{name}</h3>
-            <p className='TopicDescription'>{description}</p>
-        </div>)
-}
-const TopicRows = () => topics.map(topic => <li><Topic name = {topic.name} description={topic.description}/></li>)*/
 
 const Thread = ({name,user,timestamp,replies})=> {
     return (
@@ -121,9 +102,8 @@ const ThreadRows = () => {
             <Thread name = {thread.name} user= {thread.user} timestamp = {thread.timestamp} replies = {thread.replies}/>
         </li>))
 }
-const ThreadRowsWithNameOnly = () =>  threads.map(thread => <li><Thread name = {thread.name}/></li>)
 
-const MainContent = () => {
+const AllThreadsPage = () => {
     return (
         <div className='Main'>
             <Header className='MainThreadsListHeader' text={ 'AiheXXXXX'}/>
@@ -134,26 +114,5 @@ const MainContent = () => {
     )
 }
 
-const SideBar = () => {
-    return (
-        <div className='SideBar'>
-            <Header className='SelectedThreadsHeader'text = 'Poiminnat'/>
-            <ul className='SelectedThreadsList'>
-                {ThreadRowsWithNameOnly()}
-            </ul>
-        </div>
-    )
-}
-
-
-const AllThreadsPage = ()=>{
-    return (
-        <div className='FrontPage'>
-            <NavBar/>
-            <MainContent/>
-            <SideBar/>
-        </div>
-    )
-}
 
 export default AllThreadsPage;
